50f24505ffb5708286c5abf8d82c78efc505a651
Alex Paredes <alexparedesr95@gmail.com>
2021-08-17 23:10:11 -0500

    Se crea e integra el componente counter.

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Counter</h2>
+  <p>{{counter}} <sub>2</sub> =  {{counter * counter}}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 5
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
