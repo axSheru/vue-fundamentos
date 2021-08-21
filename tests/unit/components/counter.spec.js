38cf00df09003ddf94af91a5c8a98547be4fa545
Alex Paredes <alexparedesr95@gmail.com>
2021-08-20 23:36:58 -0500

    Se crea el archivo de testing para counter, se crea la primera prueba para corroborar el snapshot.

---
+++ tests/unit/components/counter.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import Counter from '@/components/Counter'
+
+describe('Counter Component', () => {
+    
+    test('Debe de hacer match con el snapshot.', () => {
+
+        const wrapper = shallowMount( Counter )
+        
+        expect( wrapper.html() ).toMatchSnapshot()
+
+    })
+    
+
+})
