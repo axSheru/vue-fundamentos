e5e9fee492dad157db46ae14fa73ec395336f21a
Alex Paredes <alexparedesr95@gmail.com>
2021-08-20 23:01:19 -0500

    Se implementa JEST en la primera pruebal.

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,17 +3,13 @@ describe('Example Component', () => {
   test('Debe de ser mayor a 10', () => {
     
     //Preparar
-    let value = 5
+    let value = 10
 
     //Estimular
     value = value + 2
 
     //Observar el resultado
-    if ( value > 10 ) {
-      //TODO Todo bien
-    } else {
-      throw  `${ value } no es mayor a 10`
-    }
+    expect( value ).toBeGreaterThan(10)
 
   })
   
