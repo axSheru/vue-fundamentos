f6f49f11466d8cec9003eb35cac1de8740e87765
Alex Paredes <alexparedesr95@gmail.com>
2021-08-21 00:05:05 -0500

    Se añade el test valor por defecto en etiqueta h2 en Counter component.

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -3,12 +3,24 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
     
-    test('Debe de hacer match con el snapshot.', () => {
+    /* test('Debe de hacer match con el snapshot.', () => {
 
         const wrapper = shallowMount( Counter )
         
         expect( wrapper.html() ).toMatchSnapshot()
+        
+    }) */
+    
+    test('La etiqueta h2 debe de tener el valor por defecto  "Counter"', () => {
+        
+        const wrapper = shallowMount( Counter )
 
+        expect( wrapper.find('h2').exists() ).toBeTruthy()
+
+        const h2Value = wrapper.find('h2').text()
+        
+        expect(h2Value).toBe('Counter')
+        
     })
     
 
