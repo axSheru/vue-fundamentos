438b735716dd5af6e5c003141f85bfa4f2f4f636
Alex Paredes <alexparedesr95@gmail.com>
2021-08-21 23:22:01 -0500

    Se realiza prueba para evaluar que no se dispare nada si no se escribe ? .

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -4,9 +4,11 @@ import Indecision from '@/components/Indecision'
 describe('Indecision Component', () => {
 
     let wrapper;
+    let clgSpy;
 
     beforeEach( () => {
         wrapper = shallowMount( Indecision )
+        clgSpy = jest.spyOn( console, 'log' )
     })
     
      test('Debe de hacer match con el snapshot.', () => {
@@ -15,8 +17,18 @@ describe('Indecision Component', () => {
         
     })
 
-    test('Escribir en el input no debe de disparar nada (console.log() .)', () => {
+    test('Escribir en el input no debe de disparar nada (console.log() .)', async () => {
+
+        const getAnswerSpy = jest.spyOn( wrapper.vm, 'getAnswer' )
         
+        const input = wrapper.find('input')
+
+        await input.setValue('Hola mundo')
+
+        expect( clgSpy ).toHaveBeenCalled()
+        expect( getAnswerSpy ).not.toHaveBeenCalled()
+        //expect( getAnswerSpy ).toHaveBeenCalledTimes(0)//Alternativa
+
     })
 
     test('Escribir el símbolo "?" debe de disparar el fetch.', () => {
